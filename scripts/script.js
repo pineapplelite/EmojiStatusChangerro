46f4c89182d3cdb53739212f59c6d07b9bcc86b1
pineapplelite <248683134+pineapplelite@users.noreply.github.com>
2026-01-21 12:00:45 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,19 @@
 const tg = window.Telegram.WebApp;
 
 document.getElementById("header__title").onclick = () => {
-  var requestElement = document.getElementById("main__emojiStatusAccessButton");
+  var requestElement = document.getElementById(
+    "wrapper__emojiStatusAccessButton",
+  );
   var requestElementState = window.getComputedStyle(requestElement);
   requestElement.style.display =
     requestElementState.display === "none" ? "block" : "none";
 };
 
-document.getElementById("main__setEmojiButton").onclick = () => {
-  const emojiId = document.getElementById("main__emojiInput").value;
+document.getElementById("wrapper__setEmojiButton").onclick = () => {
+  const emojiId = document.getElementById("wrapper__emojiInput").value;
   tg.setEmojiStatus(emojiId, { duration: 0 });
 };
 
-document.getElementById("main__emojiStatusAccessButton").onclick = () => {
+document.getElementById("wrapper__emojiStatusAccessButton").onclick = () => {
   tg.requestEmojiStatusAccess();
 };
